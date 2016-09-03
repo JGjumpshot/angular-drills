3406fb9f04879c951fada6b6c272d3fffbdaa30d
JGjumpshot <jackson.gunther@yahoo.com>
2016-09-03 13:32:38 -0600

    app2final

--- app2-an_array/controller.js
+++ app2-an_array/controller.js
@@ -1,4 +1,4 @@
 angular.module("arrayApp").controller('arrayController', function($scope, service) {
     //$scope.test = "I love Kaitlyn";
-    $scope.service = 
+    $scope.people = service.getData();
 })
